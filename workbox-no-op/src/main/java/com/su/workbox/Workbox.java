887c5490127d8ebc44268a969589c8e08c16b73f
su <suzhaoqiang122@163.com>
2019-07-01 14:48:25 +0800

    修正导包错误

--- workbox-no-op/src/main/java/com/su/workbox/Workbox.java
+++ workbox-no-op/src/main/java/com/su/workbox/Workbox.java
@@ -1,10 +1,10 @@
 package com.su.workbox;
 
 import android.app.Application;
-import android.app.Fragment;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 
 import okhttp3.Interceptor;
 
